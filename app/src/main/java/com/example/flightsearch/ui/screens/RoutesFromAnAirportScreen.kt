2eb5305785f090e50978a52abb9c6008b1d72788
Umesh <umesh.t.5005@gmail.com>
2024-08-17 10:05:20 +0530

    Updated RouteScreen

---
+++ app/src/main/java/com/example/flightsearch/ui/screens/RoutesFromAnAirportScreen.kt
@@ -0,0 +1,41 @@
+package com.example.flightsearch.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.flightsearch.ui.RouteDetails
+import com.example.flightsearch.ui.appuicomponents.RouteCard
+
+
+@Composable
+fun RouteScreen(
+    routes: List<RouteDetails>,
+    modifier: Modifier = Modifier,
+    addRouteToFavorites: (String, String) -> Unit,
+    removeRouteFromFavorites: (String, String) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier
+            .padding(horizontal = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        contentPadding = PaddingValues(4.dp)
+    ) {
+        items(
+            items = routes
+        ) { item->
+            RouteCard(
+                route = item,
+                addRouteToFavorites = addRouteToFavorites,
+                removeRouteFromFavorites = removeRouteFromFavorites
+            )
+        }
+    }
+}
